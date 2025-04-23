944869dd258a844a45b5f9dfb13763422dacfa9b
owusu ansah simon <94725603+Harquin14@users.noreply.github.com>
2025-04-23 07:55:02 +0200

    Update Procedure_Load_Silver.sql

--- Scripts/Silver/Procedure_Load_Silver.sql
+++ Scripts/Silver/Procedure_Load_Silver.sql
@@ -17,6 +17,25 @@ and clean silver layer data.
 =================================================================
 */
 
+/*
+=================================================================
+Stored Procedure: silver.load_silver
+Description:
+This stored procedure automates the ETL process of transforming 
+and loading cleaned data into the silver layer tables from the 
+bronze layer. It includes:
+- Data cleaning, standardization, and normalization
+- Execution time tracking
+- Error handling
+- Ensures idempotent data loading by truncating before inserting
+
+Execution:
+Run this procedure after loading bronze layer data to populate
+and clean silver layer data.
+
+=================================================================
+*/
+
 CREATE OR ALTER PROCEDURE silver.load_silver AS
 BEGIN
     DECLARE @start_time DATETIME, @end_time DATETIME;
@@ -41,79 +60,125 @@ BEGIN
         -- ================= Products =================
         PRINT('>>>> Loading silver.products');
         SET @start_time = GETDATE();
+-- Truncate the target table before inserting new data
+TRUNCATE TABLE silver.products;
 
-        TRUNCATE TABLE silver.products;
-        INSERT INTO silver.products (
-            productkey, productname, brand, color, unitcostusd, unitpriceusd, 
-            subcategorykey, subcategory, categorykey, category
-        )
-        SELECT 
-            productkey, 
-            TRIM(REPLACE(productname, '"', '')) AS productname,
-            TRIM(REPLACE(brand, '"', '')) AS brand,
-            CASE 
-                WHEN color <> TRIM(color) THEN 'null'
-                ELSE color 
-            END AS color,
-            CASE 
-                WHEN unitcostusd IN ('Proseware', 'Blue', '"$1') THEN '1'
-                ELSE REPLACE(unitcostusd, '$','')
-            END AS unitcostusd,
-            CASE 
-                WHEN TRIM(unitpriceusd) IN ('Black', 'Grey', '"$2', '"$1','Green','White','060.22 "') THEN 'null'
-                ELSE REPLACE(unitpriceusd, '$', '') 
-            END AS unitpriceusd,
-            CASE 
-                WHEN TRIM(REPLACE(subcategorykey, '"', '')) = '$3' THEN CAST('0802' AS INT)
-                WHEN TRIM(REPLACE(subcategorykey, '"', '')) IN ('899.99','184.97','099.99','592.20') THEN CAST('0304' AS INT)
-                WHEN TRIM(REPLACE(subcategorykey, '"', '')) = '$32.00' THEN CAST('0702' AS INT)
-                WHEN TRIM(REPLACE(subcategorykey, '"', '')) IN ('$72.66','$73.12') THEN CAST('0306' AS INT)
-                WHEN TRIM(REPLACE(subcategorykey, '"', '')) IN ('000.00','620.00','600.00','580.00','560.00','530.00','520.00','500.00','030.00') THEN CAST('0402' AS INT)
-                WHEN TRIM(REPLACE(subcategorykey, '"', '')) IN ('989.90','989.00','818.00','818.90','652.90','599.90','599.00','652.00') THEN CAST('0801' AS INT)
-                WHEN TRIM(REPLACE(subcategorykey, '"', '')) = '499.00' THEN CAST('0305' AS INT)
-                WHEN TRIM(REPLACE(subcategorykey, '"', '')) = '475.00' THEN CAST('0804' AS INT)
-                WHEN TRIM(REPLACE(subcategorykey, '"', '')) IN ('299.00','199.00','099.00') THEN CAST('0301' AS INT)
-                WHEN TRIM(REPLACE(subcategorykey, '"', '')) = '295.00' THEN CAST('0305' AS INT)
-                WHEN TRIM(REPLACE(subcategorykey, '"', '')) = '109.00' THEN CAST('0203' AS INT)
-                WHEN TRIM(REPLACE(subcategorykey, '"', '')) = '650.00' THEN CAST('0805' AS INT)
-                ELSE CAST(TRIM(REPLACE(subcategorykey, '"', '')) AS INT)
-            END AS subcategorykey,
-            CASE 
-                WHEN TRIM(REPLACE(subcategory, '"', '')) = '$159.00' THEN 'Printers'
-                WHEN TRIM(REPLACE(subcategory, '"', '')) = '$158.00' THEN 'Printers'
-                WHEN TRIM(REPLACE(subcategory, '"', '')) = '0201' THEN 'Monitors'
-                WHEN TRIM(REPLACE(subcategory, '"', '')) = '0301' THEN 'Laptops'
-                WHEN TRIM(REPLACE(subcategory, '"', '')) = '0305' THEN 'Projectors & Screens'
-                WHEN TRIM(REPLACE(subcategory, '"', '')) = '0405' THEN 'Digital SLR Cameras'
-                WHEN TRIM(REPLACE(subcategory, '"', '')) = '0702' THEN 'Download Games'
-                WHEN TRIM(REPLACE(subcategory, '"', '')) = '0801' THEN 'Washers & Dryers'
-                WHEN TRIM(REPLACE(subcategory, '"', '')) = '0804' THEN 'Water Heaters'
-                WHEN TRIM(REPLACE(subcategory, '"', '')) = '0805' THEN 'Coffee Machines'
-                WHEN TRIM(REPLACE(subcategory, '"', '')) = '199.99' THEN 'Refrigerators'
-                WHEN TRIM(REPLACE(subcategory, '"', '')) = '0203' THEN 'Home Theater System'
-                ELSE TRIM(REPLACE(subcategory, '"', ''))
-            END AS subcategory,
-            CASE
-                WHEN categorykey IN ('Washers & Dryers', 'Coffee Machines','0802','Water Heaters') THEN '08'
-                WHEN categorykey IN ('Laptops','Projectors & Screens',' Scanners & Fax"','0306') THEN '03'
-                WHEN categorykey = 'Camcorders' THEN '04'
-                WHEN categorykey = 'Download Games' THEN '07'
-                WHEN categorykey IN ('Televisions', 'Home Theater System') THEN '02'
-                WHEN categorykey = 'Laptops' THEN '03'
-                ELSE TRIM(categorykey)
-            END AS categorykey,
-            CASE 
-                WHEN TRIM(REPLACE(category, '"', '')) LIKE '%Music, Movies and Audio Books%' THEN 'TV and Video'
-                WHEN TRIM(REPLACE(category, '"', '')) LIKE '%03,Computers%' THEN 'Computers'
-                WHEN TRIM(REPLACE(category, '"', '')) LIKE '%02,TV and Video%' THEN 'TV and Video'
-                WHEN TRIM(REPLACE(category, '"', '')) LIKE '%Printers, Scanners & Fax%' THEN 'Computers'
-                WHEN TRIM(REPLACE(category, '"', '')) LIKE '%08,Home Appliances%' 
-                     OR TRIM(REPLACE(category, '"', '')) LIKE '%Refrigerators%' THEN 'Home Appliances'
-                WHEN TRIM(REPLACE(category, '"', '')) LIKE '%07,Games and Toys%' THEN 'Games and Toys'
-                WHEN TRIM(REPLACE(category, '"', '')) = '04,Cameras and camcorders' THEN 'Cameras and camcorders'
-                ELSE TRIM(REPLACE(category, '"', '')) 
-            END AS category
-        FROM bronze.products;
+-- Insert the cleaned data into the silver.products table
+;WITH CleanedProducts AS (
+    SELECT 
+        productkey, 
+        TRIM(REPLACE(productname, '"', '')) AS productname,
+        TRIM(REPLACE(brand, '"', '')) AS brand,
+
+        -- Cleaned color
+        CASE 
+            WHEN color <> TRIM(color) THEN NULL
+            ELSE TRIM(color)
+        END AS color,
+
+        -- Cleaned unit cost cast back to NVARCHAR(10)
+        CASE 
+            WHEN unitcostusd IN ('Proseware', 'Blue', '"$1') THEN CAST(1.00 AS NVARCHAR(10)) 
+            WHEN ISNUMERIC(REPLACE(unitcostusd, '$','')) = 1 THEN 
+                CAST(TRY_CAST(REPLACE(unitcostusd, '$','') AS DECIMAL(18,2)) AS NVARCHAR(10))
+            ELSE NULL
+        END AS unitcostusd,
+
+        -- Cleaned unit price cast back to NVARCHAR(10)
+        CASE 
+            WHEN TRIM(unitpriceusd) IN ('Black', 'Grey', '"$2', '"$1','Green','White','060.22 ') THEN NULL
+            WHEN ISNUMERIC(REPLACE(unitpriceusd, '$','')) = 1 THEN 
+                CAST(TRY_CAST(REPLACE(unitpriceusd, '$','') AS DECIMAL(18,2)) AS NVARCHAR(10))
+            ELSE NULL
+        END AS unitpriceusd,
+
+        -- Cleaned subcategorykey cast back to NVARCHAR(60)
+        CASE 
+            WHEN TRIM(REPLACE(subcategorykey, '"', '')) = '$3' THEN CAST('0802' AS NVARCHAR(60))
+            WHEN TRIM(REPLACE(subcategorykey, '"', '')) IN ('899.99','184.97','099.99','592.20') THEN CAST('0304' AS NVARCHAR(60))
+            WHEN TRIM(REPLACE(subcategorykey, '"', '')) = '$32.00' THEN CAST('0702' AS NVARCHAR(60))
+            WHEN TRIM(REPLACE(subcategorykey, '"', '')) IN ('$72.66','$73.12') THEN CAST('0306' AS NVARCHAR(60))
+            WHEN TRIM(REPLACE(subcategorykey, '"', '')) IN ('000.00','620.00','600.00','580.00','560.00','530.00','520.00','500.00','030.00') THEN CAST('0402' AS NVARCHAR(60))
+            WHEN TRIM(REPLACE(subcategorykey, '"', '')) IN ('989.90','989.00','818.00','818.90','652.90','599.90','599.00','652.00') THEN CAST('0801' AS NVARCHAR(60))
+            WHEN TRIM(REPLACE(subcategorykey, '"', '')) = '499.00' THEN CAST('0305' AS NVARCHAR(60))
+            WHEN TRIM(REPLACE(subcategorykey, '"', '')) = '475.00' THEN CAST('0804' AS NVARCHAR(60))
+            WHEN TRIM(REPLACE(subcategorykey, '"', '')) IN ('299.00','199.00','099.00') THEN CAST('0301' AS NVARCHAR(60))
+            WHEN TRIM(REPLACE(subcategorykey, '"', '')) = '295.00' THEN CAST('0305' AS NVARCHAR(60))
+            WHEN TRIM(REPLACE(subcategorykey, '"', '')) = '109.00' THEN CAST('0203' AS NVARCHAR(60))
+            WHEN TRIM(REPLACE(subcategorykey, '"', '')) = '650.00' THEN CAST('0805' AS NVARCHAR(60))
+            ELSE CAST(REPLACE(TRIM(REPLACE(subcategorykey, '"', '')), '$', '') AS NVARCHAR(60))
+        END AS subcategorykey,
+
+        -- Cleaned subcategory
+        CASE 
+            WHEN TRIM(REPLACE(subcategory, '"', '')) = '$159.00' THEN 'Printers'
+            WHEN TRIM(REPLACE(subcategory, '"', '')) = '$158.00' THEN 'Printers'
+            WHEN TRIM(REPLACE(subcategory, '"', '')) = '0201' THEN 'Monitors'
+            WHEN TRIM(REPLACE(subcategory, '"', '')) = '0301' THEN 'Laptops'
+            WHEN TRIM(REPLACE(subcategory, '"', '')) = '0305' THEN 'Projectors & Screens'
+            WHEN TRIM(REPLACE(subcategory, '"', '')) = '0405' THEN 'Digital SLR Cameras'
+            WHEN TRIM(REPLACE(subcategory, '"', '')) = '0702' THEN 'Download Games'
+            WHEN TRIM(REPLACE(subcategory, '"', '')) = '0801' THEN 'Washers & Dryers'
+            WHEN TRIM(REPLACE(subcategory, '"', '')) = '0804' THEN 'Water Heaters'
+            WHEN TRIM(REPLACE(subcategory, '"', '')) = '0805' THEN 'Coffee Machines'
+            WHEN TRIM(REPLACE(subcategory, '"', '')) = '199.99' THEN 'Refrigerators'
+            WHEN TRIM(REPLACE(subcategory, '"', '')) = '0203' THEN 'Home Theater System'
+            ELSE TRIM(REPLACE(subcategory, '"', ''))
+        END AS subcategory,
+
+        -- Cleaned categorykey cast to NVARCHAR(30)
+        CASE
+            WHEN categorykey IN ('Washers & Dryers', 'Coffee Machines','0802','Water Heaters') THEN '08'
+            WHEN categorykey IN ('Laptops','Projectors & Screens',' Scanners & Fax"','0306') THEN '03'
+            WHEN categorykey = 'Camcorders' THEN '04'
+            WHEN categorykey = 'Download Games' THEN '07'
+            WHEN categorykey IN ('Televisions', 'Home Theater System') THEN '02'
+            WHEN categorykey = 'Laptops' THEN '03'
+            ELSE TRIM(CAST(categorykey AS NVARCHAR(30)))
+        END AS categorykey,
+
+        -- Cleaned category
+        CASE 
+            WHEN TRIM(REPLACE(category, '"', '')) LIKE '%Music, Movies and Audio Books%' THEN 'TV and Video'
+            WHEN TRIM(REPLACE(category, '"', '')) LIKE '%03,Computers%' THEN 'Computers'
+            WHEN TRIM(REPLACE(category, '"', '')) LIKE '%02,TV and Video%' THEN 'TV and Video'
+            WHEN TRIM(REPLACE(category, '"', '')) LIKE '%Printers, Scanners & Fax%' THEN 'Computers'
+            WHEN TRIM(REPLACE(category, '"', '')) LIKE '%08,Home Appliances%' 
+                 OR TRIM(REPLACE(category, '"', '')) LIKE '%Refrigerators%' THEN 'Home Appliances'
+            WHEN TRIM(REPLACE(category, '"', '')) LIKE '%07,Games and Toys%' THEN 'Games and Toys'
+            WHEN TRIM(REPLACE(category, '"', '')) = '04,Cameras and camcorders' THEN 'Cameras and camcorders'
+            ELSE TRIM(REPLACE(category, '"', '')) 
+        END AS category
+    FROM bronze.products
+)
+INSERT INTO silver.products (
+    productkey, 
+    productname, 
+    brand, 
+    color, 
+    unitcostusd, 
+    unitpriceusd, 
+    subcategorykey, 
+    subcategory, 
+    categorykey, 
+    category
+)
+SELECT 
+    productkey, 
+    productname, 
+    brand, 
+    color, 
+    unitcostusd, 
+    unitpriceusd, 
+    subcategorykey, 
+    subcategory, 
+    categorykey, 
+    category
+FROM CleanedProducts
+WHERE TRY_CAST(unitpriceusd AS DECIMAL(18,2)) IS NOT NULL
+  AND TRY_CAST(unitpriceusd AS DECIMAL(18,2)) >= 0;
+
+
 
         SET @end_time = GETDATE();
         PRINT '>> LOAD DURATION: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' Seconds';
@@ -130,7 +195,8 @@ BEGIN
         INSERT INTO Silver.Sales(OrderNumber, LineItem, OrderDate, DeliveryDate, CustomerKey,StoreKey,ProductKey, Quantity, CurrencyCode)
         SELECT 
             OrderNumber, LineItem, OrderDate, DeliveryDate, CustomerKey, StoreKey,ProductKey, Quantity, CurrencyCode
-        FROM Bronze.Sales;
+        FROM Bronze.Sales
+		;
 
         SET @end_time = GETDATE();
         PRINT '>> LOAD DURATION: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' Seconds';
@@ -200,6 +266,10 @@ FROM Bronze.Customers;
     END CATCH
 END
 
+
 EXEC  Silver.Load_Silver
+
+
+
 
 
